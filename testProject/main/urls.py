ba629f0746eff308fbd8a62d188c653bf6365ce6
tanaliga100 <43674051+tanaliga100@users.noreply.github.com>
2024-03-02 00:21:30 +0800

    learn the query-set

--- testProject/main/urls.py
+++ testProject/main/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("<str:name>", views.index, name="index"),
     path("v1/", views.v1, name="index")
 ]
-
--- testProject/main/views.py
+++ testProject/main/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import TodoList, Item
 
 # Create your views here.
 
-def index(response):
-   return HttpResponse("<h1>Tech with jordan100</h1>")
+def index(response, name):
+   todo_list = TodoList.objects.get(name=name)
+   items = todo_list.item_set.all()
+
+   items_text = ",</br> ".join([item.text for item in items]);
+
+   return HttpResponse(f"<h1>{todo_list.name}</h1><p>{items_text}</p>")
 
 def v1(response):
    return HttpResponse("<h1>View 1</h1>")
