dc19cde87daa703acf7bacb79eb2ed4c999a0276
tanaliga100 <43674051+tanaliga100@users.noreply.github.com>
2024-03-06 18:19:42 +0800

    query-sets

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member
+from django.db.models import Q
 
 # Create your views here.
 
@@ -13,14 +14,29 @@ from .models import Member
 #     return HttpResponse(template.render(context, request))
 
 def testing(request):
-  template = loader.get_template("template.html")
-  mymembers = Member.objects.all().values()
-  context = {
-    'fruits': ['Apple', 'Banana', 'Cherry'],
-    'name': "jordantanaliga100",
-    'lists': mymembers
-  }
-  return HttpResponse(template.render(context, request))
+    template = loader.get_template("template.html")
+    mymembers = Member.objects.all().values()
+    mydata = Member.objects.all()
+    values_list = Member.objects.values_list('firstname')
+    filtered = Member.objects.filter(firstname='jordan100').values()
+    filtered_mul = Member.objects.filter(firstname="jordan100").values() | Member.objects.filter(lastname="tejoso100").values()
+    filtered_q = Member.objects.filter(Q(firstname="chris100") | Q(lastname="tejoso100")) 
+    filtered_char = Member.objects.filter(firstname__startswith="jr").values()
+    order_by = Member.objects.all().order_by('firstname').values()
+    context = {
+        "fruits": ["Apple", "Banana", "Cherry"],
+        "name": "jordantanaliga100",
+        "lists": mymembers,
+        "mymembers": mydata,
+        "values_list": values_list,
+        "filtered": filtered,
+        "filtered_mul": filtered_mul,
+        "filtered_q": filtered_q,
+        "filtered_char": filtered_char,
+        "order_by": order_by,
+    }
+
+    return HttpResponse(template.render(context, request))
 
 def main(request): 
     template = loader.get_template("main.html")
@@ -41,4 +57,3 @@ def details(request, id):
       'mymember': mymember,
     } 
     return HttpResponse(template.render(context, request))
-   
